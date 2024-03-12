a776dffbaad3be3d38e71d43a31e1f6ff16709f3
staakaar <vancyroletaka@gmail.com>
2024-03-13 01:22:48 +0900

    add sequentially_consistent fence

---
+++ src/fence.rs
@@ -0,0 +1,27 @@
+use std::sync::atomic::fence;
+
+static mut DATA: [u64;10] = [0; 10];
+
+const ATOMIC_FALSE: AtomicBool = AtomicBool::new(false);
+static READY: [AtomicBool; 10] = [ATOMIC_FALSE; 10];
+
+fn main() {
+    for i in 0..10 {
+        thread::spawn(move || {
+            let data = some_calculation(i);
+            unsafe { DATA[i] = data };
+            READY[i].store(true, Release);
+        });
+    }
+
+    thread::sleep(Duration::from_millis(500));
+    let ready: [bool; 10] = std::array::from_fn(|i| READY[i].load(Relaxed));
+    if ready.contains(&true) {
+        fence(Acquire);
+        for i in 0..10 {
+            if ready[i] {
+                println!("data{i} = {}", unsafe { DATA[i] });
+            }
+        }
+    }
+}
