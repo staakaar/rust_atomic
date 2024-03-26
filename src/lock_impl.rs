38e92cacd56586a322d3bdc0e308b39b02a671a3
staakaar <vancyroletaka@gmail.com>
2024-03-27 01:12:45 +0900

    chore: add lock_impl.rs

---
+++ src/lock_impl.rs
@@ -0,0 +1,39 @@
+use atomic_wait::{wait, wake_one, wake_all};
+
+pub struct Mutex<T> {
+    // 0: unlocked
+    // 1: locked
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+// 安全なロックインターフェース
+pub struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+}
+
+unsafe impl<T> Sync for MutexGuard<'_, T> where T: Sync {}
+// スレッド間共有できるように
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.value.get() }
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.value.get() }
+    }
+}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+}
