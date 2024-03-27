750ca156449196218d6b5ef614c9bbc6599f7b11
staakaar <vancyroletaka@gmail.com>
2024-03-28 00:55:06 +0900

    chore: add lock method

--- src/lock_impl.rs
+++ src/lock_impl.rs
@@ -29,11 +29,32 @@ impl<T> DerefMut for MutexGuard<'_, T> {
     }
 }
 
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        // stateを0(アンロック)に戻す
+        self.mutex.state.store(0, Release);
+        // 待機中のスレッドがあればそのひとつを起こす
+        wake_one(&self.mutex.state);
+    }
+}
+
 impl<T> Mutex<T> {
     pub const fn new(value: T) -> Self {
         Self {
             state: AtomicU32::new(0),
             value: UnsafeCell::new(value),
         }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        // stateを１にセット
+        while self.state.swap(1, Acquire) == 1 {
+            // すでにロック済みの場合、stateが１でなくなるまで待機する
+            // 複数スレッドが待機していたとしてもロックできるのは1つだけなため
+            // 複数同時に起こしたとしてもプロセッサの時間を費やすだけで１以外のスレッドはまた元に戻る
+            // ただ起こしたスレッドがロックを獲得するとは限らない
+            wait(&self.state, 1);
+        }
+        MutexGuard { mutex: self }
     }
 }
